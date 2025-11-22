3744a05600a980a690a85a843464c12762f7e663
system205 <arsenmutalapov205@gmail.com>
2025-11-22 14:02:25 +0300

    Refactor test to check Extra column is in the table

--- frontend/peer-pilot/src/instructor/grading/Dashboard.test.tsx
+++ frontend/peer-pilot/src/instructor/grading/Dashboard.test.tsx
@@ -1,10 +1,15 @@
-    // src/components/MyComponent.test.tsx
-    import { render, screen } from '@testing-library/react';
-    import Dashboard from './Dashboard';
+// src/components/MyComponent.test.tsx
+import { render, screen, within } from '@testing-library/react';
+import Dashboard from './Dashboard';
 
-    describe('Renders extra column on the teams view of instructors dashboard', () => {
-      it('renders the correct text', () => {
-        render(<Dashboard />);
-        expect(screen.getAllByText('Extra')[0]).toBeInTheDocument();
-      })
+describe('Renders extra column on the teams view of instructors dashboard', () => {
+    it('should display "Extra" column header in the main grades table', () => {
+        render(<Dashboard/>);
+
+        const extraHeader = screen.getAllByText('Extra')[0];
+        expect(extraHeader).toBeInTheDocument();
+
+        const table = screen.getByRole('table');
+        expect(within(table).getAllByText('Extra').length).toBeGreaterThan(0);
     });
+});
